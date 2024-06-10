44f626024b1f03b0dbbb0c720cb46a93371e30b7
Abhyuday-Dev <abhyuday7176@hotmail.com>
2024-06-11 00:36:00 +0530

    made home page

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div style={{ display: 'flex', alignItems: 'center', height: '90vh' }}>
+      <div style={{ flex: '1', textAlign: 'center' }}>
+        <h1>Welcome to the Course Site!</h1>
+        <p>Learn, Explore, and Grow with Us</p>
+      </div>
+      <div style={{ flex: '1', textAlign: 'center' }}>
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/002/779/389/original/student-woman-with-laptop-studying-on-online-course-online-education-concept-illustration-flat-vector.jpg"
+          alt="Course Site Image"
+          style={{ height: '100%', maxWidth: '100%' }}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,164 +5,165 @@ const { authenticateJwt, SECRET } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/me",authenticateJwt,async(req,res)=>{
-    const user=await User.findOne({username: req.user.username});
-    if(!user) {
-        res.status(403).json({message:"Admin Dosen't exist"});
-        return;
-    }
-    res.json({
-        username: user.username,
-        name:user.name,
-        userId: user._id
-    })
+router.get("/me", authenticateJwt, async (req, res) => {
+  const user = await User.findOne({ username: req.user.username });
+  if (!user) {
+    res.status(403).json({ message: "Admin Dosen't exist" });
+    return;
+  }
+  res.json({
+    username: user.username,
+    name: user.name,
+    userId: user._id,
+  });
 });
 
 router.post("/signup", async (req, res) => {
-  const {name,username, password } = req.body;
-  console.log(req.body);
+  const { name, username, password } = req.body;
   const user = await User.findOne({ username });
   if (user) {
     res.status(403).json({ message: "User Already Exists" });
   } else {
-    const newUser = new User({ name,username, password });
+    const newUser = new User({ name, username, password });
     await newUser.save();
     const token = jwt.sign({ username, role: "user" }, SECRET, {
       expiresIn: "2h",
     });
-    res.json({ message: "User Created Successfully",token: token });
+    res.json({ message: "User Created Successfully", token: token });
   }
 });
 
 router.post("/login", async (req, res) => {
   const { credentials } = req.body;
 
-  const username=credentials.username;
-  const password=credentials.password;
+  const username = credentials.username;
+  const password = credentials.password;
   const user = await User.findOne({ username, password });
   if (user) {
     const token = jwt.sign({ username, role: "user" }, SECRET, {
       expiresIn: "2h",
     });
-    res.json({ message: "User Logged In successfully",token: token });
+    res.json({ message: "User Logged In successfully", token: token });
   } else {
     res.status(403).json({ message: "User does not exist" });
   }
 });
 
-router.get("/courses",authenticateJwt,async(req, res) => {
-    console.log("hehr")
-    const courses=await Course.find();
-    console.log(courses);
-    res.json({ courses: courses });
+router.get("/courses", authenticateJwt, async (req, res) => {
+  console.log("hehr");
+  const courses = await Course.find();
+  res.json({ courses: courses });
 });
 
-router.post('/addCourse', async (req, res) => {
-    try {
-      const newCourse = new Course(req.body);
-      await newCourse.save();
-      res.status(201).json(newCourse);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
-
-
-  router.get("/courses/:courseId",authenticateJwt,async(req,res)=>{
-    const courseId=req.params.courseId;
-    const course=await Course.findById(courseId);
-    if(course){
-        res.json({course})
-    }
-    else{
-        res.status(404).json({message:"Course Not Found"});
-    }
+router.post("/addCourse", async (req, res) => {
+  try {
+    const newCourse = new Course(req.body);
+    await newCourse.save();
+    res.status(201).json(newCourse);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-  
-router.post('/courses/:courseId/enroll',authenticateJwt, async (req, res) => {
-    const { courseId } = req.params;
-    const { userId } = req.body;
-  
-    try {
-      const course = await Course.findById(courseId);
-      if (!course) {
-        return res.status(404).json({ message: 'Course not found' });
-      }
-      // Check if user is already enrolled
-      if (course.enrolledUsers.includes(userId)) {
-        return res.status(400).json({ message: 'User already enrolled in this course' });
-      }
-  
-      // Update course's enrolledUsers array and save the course
-      course.enrolledUsers.push(userId);
-      await course.save();
-  
-      // Update user's courses array and save the user
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      user.courses.push(courseId);
-      await user.save();
-  
-      res.json({ message: 'User enrolled in course successfully' });
-    } catch (error) {
-      console.error('Error enrolling user in course:', error);
-      res.status(500).json({ message: 'Internal server error' });
+router.get("/courses/:courseId", authenticateJwt, async (req, res) => {
+  const courseId = req.params.courseId;
+  const course = await Course.findById(courseId);
+  if (course) {
+    res.json({ course });
+  } else {
+    res.status(404).json({ message: "Course Not Found" });
+  }
+});
+
+router.post("/courses/:courseId/enroll", authenticateJwt, async (req, res) => {
+  const { courseId } = req.params;
+  const { userId } = req.body;
+
+  try {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
     }
-  });
-
-  router.get('/getUserCourses/:userId',authenticateJwt, async (req, res) => {
-    try {
-      const { userId } = req.params;
-  
-      // Fetch the user by userId and populate courses and completedCourses
-      const user = await User.findById(userId)
-        .populate('courses')
-        .populate('completedCourses');
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Send back the arrays of courses and completedCourses
-      res.json({ enrolledCourses: user.courses, completedCourses: user.completedCourses });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
+    // Check if user is already enrolled
+    if (course.enrolledUsers.includes(userId)) {
+      return res
+        .status(400)
+        .json({ message: "User already enrolled in this course" });
     }
-  });
-  
 
-  router.post('/markCourseCompleted', async (req, res) => {
-    try {
-      const { userId, courseId } = req.body;
-  
-      // Find the user and update their completedCourses array
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Check if the course is in the user's enrolled courses
-      if (!user.courses.includes(courseId)) {
-        return res.status(400).json({ message: 'User is not enrolled in this course' });
-      }
-  
-      // Move the course from enrolled to completed
-      user.courses = user.courses.filter(course => course.toString() !== courseId);
-      user.completedCourses.push(courseId);
-  
-      await user.save();
-      res.json({ message: 'Course marked as completed' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
+    // Update course's enrolledUsers array and save the course
+    course.enrolledUsers.push(userId);
+    await course.save();
+
+    // Update user's courses array and save the user
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
-  
+    user.courses.push(courseId);
+    await user.save();
 
+    res.json({ message: "User enrolled in course successfully" });
+  } catch (error) {
+    console.error("Error enrolling user in course:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
-  module.exports = router
+router.get("/getUserCourses/:userId", authenticateJwt, async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    // Fetch the user by userId and populate courses and completedCourses
+    const user = await User.findById(userId)
+      .populate("courses")
+      .populate("completedCourses");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Send back the arrays of courses and completedCourses
+    res.json({
+      enrolledCourses: user.courses,
+      completedCourses: user.completedCourses,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.post("/markCourseCompleted", async (req, res) => {
+  try {
+    const { userId, courseId } = req.body;
+
+    // Find the user and update their completedCourses array
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check if the course is in the user's enrolled courses
+    if (!user.courses.includes(courseId)) {
+      return res
+        .status(400)
+        .json({ message: "User is not enrolled in this course" });
+    }
+
+    // Move the course from enrolled to completed
+    user.courses = user.courses.filter(
+      (course) => course.toString() !== courseId
+    );
+    user.completedCourses.push(courseId);
+
+    await user.save();
+    res.json({ message: "Course marked as completed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
